507da8380e08862269c50ff31f1b93e988a8d4a1
Wanderson Timóteo <wanderson_timoteo@outlook.com>
2024-11-19 11:15:37 -0400

    corrigido fastasminhas

--- source/ghosts/Ghosts.js
+++ source/ghosts/Ghosts.js
@@ -2,44 +2,46 @@
  * The Ghosts Class
  */
 class Ghosts {
+    
     /**
      * The Ghosts constructor
      * @param {?oldManager} Ghosts
      */
     constructor(oldManager) {
         // Ghosts Data
-        this.globalMode = "scatter"; // Global Mode
-        this.modeCounter = 0; // Amount of switchs between Scatter-Chase
-        this.modeTimer = Data.getSwitchTime(0); // Scatter/Chase timer
-        this.frightTimer = 0; // Frigthen timer
-        this.blinksCount = 0; // Amount of blinks at frighten end
-        this.eyesCounter = 0; // Amount of dead Ghost during a fright mode
-
+        this.globalMode  = "scatter";                   // Global Mode
+        this.modeCounter = 0;                           // Amount of switchs between Scatter-Chase
+        this.modeTimer   = Data.getSwitchTime(0);       // Scatter/Chase timer
+        this.frightTimer = 0;                           // Frigthen timer
+        this.blinksCount = 0;                           // Amount of blinks at frighten end
+        this.eyesCounter = 0;                           // Amount of dead Ghost during a fright mode
+        
         // The Ghosts
-        var canvas = Board.gameCanvas;
-        this.blinky = new Blinky(canvas, oldManager ? oldManager.blinky.getDots() : null);
-        this.pinky = new Pinky(canvas, oldManager ? oldManager.pinky.getDots() : null);
-        this.inky = new Inky(canvas, oldManager ? oldManager.inky.getDots() : null, this.blinky);
-        this.clyde = new Clyde(canvas, oldManager ? oldManager.clyde.getDots() : null);
-        this.ghosts = [this.blinky, this.pinky, this.inky, this.clyde];
-
+        var canvas       = Board.gameCanvas;
+        this.blinky      = new Blinky(canvas, oldManager ? oldManager.blinky.getDots() : null);
+        this.pinky       = new Pinky(canvas, oldManager ? oldManager.pinky.getDots() : null);
+        this.inky        = new Inky(canvas, oldManager ? oldManager.inky.getDots() : null, this.blinky);
+        this.clyde       = new Clyde(canvas, oldManager ? oldManager.clyde.getDots() : null);
+        this.ghosts      = [ this.blinky, this.pinky, this.inky, this.clyde ];
+        
         // Pen Data
-        this.penType = !!oldManager; // Type used to force ghosts out of the pen (false = using
-        // ... ghost's dot counters | true = using global dot counter)
-        this.penTimer = 0; // Pen Leaving Force Timer
-        this.globalDots = 0; // Global dots counter
-        this.inPen = [this.pinky, this.inky, this.clyde];
-
+        this.penType     = !!oldManager;               // Type used to force ghosts out of the pen (false = using
+                                                       // ... ghost's dot counters | true = using global dot counter)
+        this.penTimer    = 0;                          // Pen Leaving Force Timer
+        this.globalDots  = 0;                          // Global dots counter
+        this.inPen       = [ this.pinky, this.inky, this.clyde ];
+        
         if (!this.penType) {
             this.inPen.forEach(() => this.checkDotLimit());
         }
     }
-
+    
+    
     /**
      * Animates all the Ghosts, and reduces the ghosts modes timers
      * @param {number} time
      * @param {number} speed
-     * @param {Blob} blob
+     * @param {Blob}   blob
      */
     animate(time, speed, blob) {
         if (this.frightTimer > 0) {
@@ -47,12 +49,12 @@ class Ghosts {
         } else if (this.modeCounter < Data.totalSwitchs && this.modeTimer > 0) {
             this.modeTimer -= time;
         }
-
+        
         this.switchMode(blob);
         this.move(speed, blob);
         this.increasePenTimer(time);
     }
-
+    
     /**
      * Changes the Ghosts mode
      * @param {Blob} blob
@@ -61,38 +63,38 @@ class Ghosts {
         let oldMode = this.globalMode;
         if (Data.isFrighten(this.globalMode) && this.frightTimer <= 0) {
             this.blinksCount -= 1;
-
+            
             if (this.blinksCount >= 0) {
                 this.frightTimer = Data.blinksTimer;
-                this.globalMode = this.globalMode === "white" ? "blue" : "white";
+                this.globalMode  = this.globalMode === "white" ? "blue" : "white";
             } else {
-                this.globalMode = this.getSwitchMode();
+                this.globalMode  = this.getSwitchMode();
             }
             this.switchGhostsMode(oldMode, blob);
-
+        
         } else if (this.modeTimer <= 0) {
             this.modeCounter += 1;
-            this.globalMode = this.getSwitchMode();
-            this.modeTimer = Data.getSwitchTime(this.modeCounter);
+            this.globalMode   = this.getSwitchMode();
+            this.modeTimer    = Data.getSwitchTime(this.modeCounter);
             this.switchGhostsMode(oldMode, blob);
         }
     }
-
+    
     /**
      * Changes the mode of each Ghost
      * @param {number} oldMode
-     * @param {Blob} blob
+     * @param {Blob}   blob
      */
     switchGhostsMode(oldMode, blob) {
         this.ghosts.forEach((ghost) => {
             ghost.switchMode(oldMode, this.globalMode, blob);
         });
     }
-
+    
     /**
      * Moves all the Ghosts
      * @param {number} speed
-     * @param {Blob} blob
+     * @param {Blob}   blob
      */
     move(speed, blob) {
         let mode = this.getSwitchMode();
@@ -102,14 +104,14 @@ class Ghosts {
             }
         });
     }
-
+    
     /**
      * Draws all The Ghosts
      */
     draw() {
         this.ghosts.forEach((ghost) => ghost.draw());
     }
-
+    
     /**
      * Sets the Ghosts targets when the Blob reached a new Tile
      * @param {Blob} blob
@@ -121,7 +123,7 @@ class Ghosts {
             }
         });
     }
-
+    
     /**
      * Sets Blinky's "Cruise Elroy" Mode when the number of dots left reaches the target
      * @param {number} dots
@@ -129,25 +131,25 @@ class Ghosts {
     checkElroyDots(dots) {
         this.blinky.checkElroyDots(dots);
     }
-
+    
     /**
      * Switches to Frighten mode
      * @param {Blob} blob
      */
     frighten(blob) {
-        var oldMode = this.globalMode;
-        this.globalMode = "blue";
+        var oldMode      = this.globalMode;
+        this.globalMode  = "blue";
         this.frightTimer = Data.getFrightTime();
         this.blinksCount = Data.getBlinks();
         this.eyesCounter = 0;
-
+        
         this.switchGhostsMode(oldMode, blob);
     }
-
+    
     /**
      * The Ghost kills the Blob or Dies from it. Returns true if the Blob died
      * @param {{x: number, y: number}} blobTile
-     * @param {function} onKill
+     * @param {function} onKll
      * @param {function} onDie
      */
     crash(blobTile, onKill, onDie) {
@@ -162,7 +164,7 @@ class Ghosts {
             return !!result;
         });
     }
-
+    
     /**
      * Returns the current Scatter or Chase mode
      * @return {string}
@@ -170,7 +172,7 @@ class Ghosts {
     getSwitchMode() {
         return this.modeCounter % 2 === 0 ? "scatter" : "chase";
     }
-
+    
     /**
      * Returns the current Mode, including the Fright variations
      * @return {number}
@@ -178,7 +180,7 @@ class Ghosts {
     getMode() {
         return this.globalMode;
     }
-
+    
     /**
      * Returns true if the current mode is a Fright
      * @return {boolean}
@@ -186,7 +188,8 @@ class Ghosts {
     areFrighten() {
         return Data.isFrighten(this.globalMode);
     }
-
+    
+    
     /**
      * Increases the global dots or the first Ghost internal dots depending on the mode
      */
@@ -197,7 +200,7 @@ class Ghosts {
             this.incGlobalDots();
         }
     }
-
+    
     /**
      * Increases the internal dots counter for the Ghost in the Pen array
      */
@@ -207,37 +210,37 @@ class Ghosts {
             this.checkDotLimit();
         }
     }
-
+    
     /**
      * Checks if a ghost can leave pen
      */
     checkDotLimit() {
         let limits = Data.getLevelData("penLeavingLimit"),
-            ghost = this.inPen[0];
-
+            ghost  = this.inPen[0];
+        
         if (limits[ghost.getID()] <= ghost.getDots()) {
             this.releaseGhostFromPen();
         }
     }
-
+    
     /**
      * Increases the global dot counter and release ghosts changes type when required
      */
     incGlobalDots() {
         this.globalDots += 1;
-
+        
         this.inPen.forEach((ghost) => {
             if (this.globalDots === Data.getPenDotsCount(ghost.getID())) {
                 if (ghost.getID() <= 2) {
                     this.releaseGhostFromPen();
                 } else {
-                    this.penType = false;
+                    this.penType    = false;
                     this.globalDots = 0;
                 }
             }
         });
     }
-
+    
     /**
      * Increases the Pen Timer
      * @param {number} time
@@ -249,7 +252,7 @@ class Ghosts {
             this.penTimer = 0;
         }
     }
-
+    
     /**
      * Resents the Pen Timer to cero, since the Blob ate a pill and checks the Dots counters
      */
@@ -257,7 +260,7 @@ class Ghosts {
         this.penTimer = 0;
         this.incDotCounter();
     }
-
+    
     /**
      * Releases the first Ghost in the vector from Pen
      */
@@ -265,10 +268,10 @@ class Ghosts {
         let ghost = this.inPen[0];
         ghost.setPath("exitPen");
         ghost.activateElroy();
-
+        
         this.inPen = this.inPen.slice(1);
     }
-
+    
     /**
      * Adds the given Ghost to Pen
      * @param {Ghost} ghost
@@ -284,11 +287,10 @@ class Ghosts {
             }
             this.inPen.splice(i, 0, ghost);
             ghost.setPath("inPen");
-
-            if (this.penType) {
+            
+            if (!this.penType) {
                 this.checkDotLimit();
             }
         }
     }
 }
-
